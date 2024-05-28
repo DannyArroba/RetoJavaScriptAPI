77b5a018825ea8f4157440ba89bfff5b9c8c822c
Bryan11L <danny.gamer11@hotmail.com>
2024-05-27 21:58:18 -0500

    FINALEXTRA

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,52 @@
-let nombre="Danny Arroba";
-
-let num1=5;
-let num2="5";
-let num3=6;
-let num4=8;
-console.log(nombre);
-///compara el valor
-console.log(num1===num2);
-///compara el mismo tipo de dato 5 o "5" no es igual
-console.log(num1===num2);
-
-//compara el valor y el tipo de dato
-//!== y el ===
-
-//& todas verdadera
-//or una de ellas que sea 
-
-console.log(num1==num2 && num3!=num4);
-
-console.log(num1===num2 || num3!=num4);
-
-function Sumar(num1,num2) {
-let res=num1+num2;
-console.log(res);   
-}
-
-Sumar(1,5);
+async function getTrendingMoviesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+  
+    const movies = data.results;
+    movies.forEach(movie => {
+      const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview');
+  
+      const movieContainer = document.createElement('div');
+      movieContainer.classList.add('movie-container', 'bg-white', 'p-4', 'rounded', 'shadow');
+  
+      const movieImg = document.createElement('img');
+      movieImg.classList.add('w-1/3', 'h-32');
+      movieImg.setAttribute('alt', movie.title);
+      movieImg.setAttribute(
+        'src',
+        'https://image.tmdb.org/t/p/w300' + movie.poster_path,
+      );
+  
+      const information = document.createElement('div');
+      information.classList.add('mt-4');
+  
+      const titulo = document.createElement('h3');
+      titulo.classList.add('text-lg', 'font-bold', 'text-center');
+      titulo.innerText = movie.title;
+  
+      const description = document.createElement('p');
+      description.classList.add('text-gray-600');
+      description.setAttribute('style', 'max-width: 300px; overflow: hidden; text-overflow: ellipsis;');
+      description.innerText = movie.overview;
+     
+  
+      const popularity = document.createElement('h3');
+      popularity.classList.add('text-lg', 'font-bold', 'text-center');
+      popularity.innerText = movie.popularity;
+  
+      const release_date = document.createElement('h2');
+      release_date.classList.add('text-gray-600', 'mt-2', 'text-center');
+      release_date.innerText = movie.release_date;
+  
+      movieContainer.appendChild(movieImg);
+      movieContainer.appendChild(information);
+      information.appendChild(titulo);
+      information.appendChild(description);
+      information.appendChild(popularity);
+      information.appendChild(release_date);
+      trendingPreviewMoviesContainer.appendChild(movieContainer);
+    });
+  }
+  
+  getTrendingMoviesPreview();
+  
